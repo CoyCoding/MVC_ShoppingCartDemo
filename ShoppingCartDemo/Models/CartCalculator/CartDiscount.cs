063dbf5f4159f670f0f2d71ba03377768aa4e52f
matt coy <fluffytoycoy@gmail.com>
2018-11-15 01:45:16 -0600

    added CartDiscount and contaning folder

---
+++ ShoppingCartDemo/Models/CartCalculator/CartDiscount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartDemo.Models.CartCalculator
+{
+    public class CartDiscount : ICartDiscount
+    {
+        public decimal Applydiscount(decimal cartTotal)
+        {
+            return (cartTotal - (10m / 100m * cartTotal));
+        }
+    }
+}
---
+++ ShoppingCartDemo/Models/CartCalculator/ICartDiscount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDemo.Models.CartCalculator
+{
+    public interface ICartDiscount
+    {
+        decimal Applydiscount(decimal cartTotal);
+    }
+}
