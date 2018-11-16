81e0221ce0c57bd6e64811d7d9fa85a048c1cad1
matt coy <fluffytoycoy@gmail.com>
2018-11-15 21:49:06 -0600

    added ninject for testing

---
+++ ShoppingCartDemoTest/DependencyInject/NinjectDIResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using ShoppingCartDomain.Entities;
+using Ninject;
+
+namespace ShoppingCartDemo.UnitTest.DependencyInject
+{
+    public class NinjectDIResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDIResolver(IKernel kernelPeram)
+        {
+            kernel = kernelPeram;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //add bindings
+        }
+
+    }
+}
---
+++ ShoppingCartDomain/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDomain.Entities
+{
+        public class Product
+        {
+            public int id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public decimal Price { get; set; }
+
+            public int Quantity { get; set; }
+
+            public string Category { get; set; }
+
+            public string Seller { get; set; }
+        }
+    }
+
