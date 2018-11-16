fa24b3ebd7f752b0edf877a7fe4517d9fcd53a2e
matt coy <fluffytoycoy@gmail.com>
2018-11-16 00:07:25 -0600

    db Connection is working

--- ShoppingCartDemo/DependencyInject/NinjectDIResolver.cs
+++ ShoppingCartDemo/DependencyInject/NinjectDIResolver.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using ShoppingCartDemo.Models;
 using Moq;
 using ShoppingCartDomain.Entities;
+using ShoppingCartDomain.DbAccess;
 using Ninject;
 using ShoppingCartDemo.Models.CartCalculator;
 
@@ -36,16 +37,7 @@ namespace ShoppingCartDemo.DependencyInject
             kernel.Bind<ITotalCalculator>().To<TotalCalculator>();
             kernel.Bind<ICartDiscount>().To<CartDiscount>();
 
-            Mock<IProductRepo> mock = new Mock<IProductRepo>();
-
-            mock.Setup(obj => obj.Products).Returns(new List<Product>
-            {
-                new Product{ Name = "Food", Price = 2m},
-                new Product{ Name = "Game", Price = 50m},
-                new Product{ Name = "Clothes", Price = 20m}
-            });
-
-            kernel.Bind<IProductRepo>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepo>().To<DbProductRepo>();
         }
 
     }
---
+++ ShoppingCartDomain/DbAccess/DbProductRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCartDomain.Entities;
+
+namespace ShoppingCartDomain.DbAccess
+{
+    public class DbProductRepo : IProductRepo
+    {
+        private DbConnectionContext _context = new DbConnectionContext();
+
+        public  IEnumerable<Product> Products
+        {
+            get { return _context.Products; }
+        }
+    }
+
+}
