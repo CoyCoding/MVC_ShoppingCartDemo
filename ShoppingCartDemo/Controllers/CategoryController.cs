28ef426f422af1e0795d59e5d1195d55a6bea0a8
matt coy <fluffytoycoy@gmail.com>
2018-11-18 10:51:14 -0600

    Added sidebar

---
+++ ShoppingCartDemo/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ShoppingCartDomain.Entities;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartDemo.Models;
+
+namespace ShoppingCartDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _productRepo;
+        // GET: Category
+
+        public CategoryController()
+        {
+            _productRepo = new ApplicationDbContext();
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = _productRepo.Categories
+                .Select(cat => cat.Name)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+    }
+}
