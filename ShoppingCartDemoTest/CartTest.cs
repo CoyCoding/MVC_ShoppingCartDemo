6dbadc14005aeb104271705017225c6a49f80af9
matt coy <fluffytoycoy@gmail.com>
2018-11-19 11:57:13 -0600

    cart test pass

---
+++ ShoppingCartDemoTest/CartTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCartDomain.Entities;
+
+namespace ShoppingCartDemoTest
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void Can_Add_Items()
+        {
+            //Arrange 
+            Product p1 = new Product { Id = 1, Name = "P1" };
+            Product p2 = new Product { Id = 2, Name = "P2" };
+
+            //Arrange
+            Cart cart = new Cart();
+
+            //Act
+            cart.AddItem(p1, 1);
+            CartItem[] list = cart.GetCartItems.ToArray();
+            //Assert
+            Assert.AreEqual(list[0].Product, p1);
+            //Act
+            cart.RemoveSingleItem(p1);
+            Assert.AreEqual(cart.CartValueTotal(), 0);
+            
+            
+
+        }
+    }
+}
---
+++ ShoppingCartDomain/Entities/Cart.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDomain.Entities
+{
+    public class Cart
+    {
+        private List<CartItem> productCollection = new List<CartItem>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartItem cartItem = productCollection
+                .Where(p => p.Product.Id == product.Id)
+                .FirstOrDefault();
+
+            if(cartItem == null)
+            {
+                productCollection.Add(new CartItem
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                cartItem.Quantity += quantity;
+            }
+        }
+
+        public void RemoveSingleItem(Product product)
+        {
+            int itemIndex = productCollection.FindIndex(item => item.Product.Id == product.Id);
+            if (itemIndex >= 0)
+            {
+                if (--productCollection.ToList()[itemIndex].Quantity == 0)
+                {
+                    productCollection.RemoveAt(itemIndex);
+                }
+            }
+        }
+
+        public decimal CartValueTotal()
+        {
+            return productCollection.Sum(cart => cart.Product.Price * cart.Quantity);
+        }
+
+        public void Clear()
+        {
+            productCollection.Clear();
+        }
+
+        public IEnumerable<CartItem> GetCartItems
+        {
+            get { return productCollection; }
+        }
+    }
+}
