bac205d9c464c6b8e2460beed77143a2cf60bd2e
matt coy <fluffytoycoy@gmail.com>
2018-11-29 20:27:20 -0600

    added DI for email settings and web.config

--- ShoppingCartDemo/DependencyInject/NinjectDIResolver.cs
+++ ShoppingCartDemo/DependencyInject/NinjectDIResolver.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ShoppingCartDemo.Models;
 using Moq;
 using ShoppingCartDomain.Entities;
+using ShoppingCartDomain.Abstract;
 using ShoppingCartDemo.Models.ViewModel;
 using Ninject;
 using ShoppingCartDemo.Models.CartCalculator;
@@ -37,6 +39,14 @@ namespace ShoppingCartDemo.DependencyInject
             kernel.Bind<ITotalCalculator>().To<TotalCalculator>();
             kernel.Bind<ICartDiscount>().To<CartDiscount>();
             kernel.Bind<IApplicationDbContext>().To<ApplicationDbContext>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
         }
 
     }
