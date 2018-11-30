43393dbd1f9a7600b08e24b6bace1b14cd98b2bc
matt coy <fluffytoycoy@gmail.com>
2018-11-29 20:10:24 -0600

    Email Order and Settings for sending email when order is placed

---
+++ ShoppingCartDomain/Entities/EmailOrderProcessor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCartDomain.Abstract;
+
+namespace ShoppingCartDomain.Entities
+{
+    public class EmailOrderProcessor : IOrderProcessor 
+    {
+        private EmailSettings emailSettings;
+
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            emailSettings = settings;
+        }
+
+        public void ProcessOrder(Cart cart, ShippingDetails shippingInfo)
+        {
+            using(var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.SeverName;
+                smtpClient.Port = emailSettings.SeverPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential
+                    (emailSettings.Username, emailSettings.Password);
+
+                if (emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                    .AppendLine("A new order has been made")
+                    .AppendLine("_--")
+                    .AppendLine("Items:");
+
+                foreach(var item in cart.GetCartItems)
+                {
+                    var subtotal = item.Product.Price * item.Quantity;
+                    body.AppendFormat("{0}   Quantity - {1} (subtotal: {2:c})", item.Product.Name, item.Quantity, subtotal);
+                }
+
+                body.AppendFormat("Total order value: {0:c}", cart.CartValueTotal())
+                    .AppendLine("---")
+                    .AppendLine("Shipping to:")
+                    .AppendLine(shippingInfo.Name)
+                    .AppendLine(shippingInfo.Line1)
+                    .AppendLine(shippingInfo.Line2 ?? "")
+                    .AppendLine(shippingInfo.Line3 ?? "")
+                    .AppendLine(shippingInfo.City)
+                    .AppendLine(shippingInfo.State ?? "")
+                    .AppendLine(shippingInfo.Country)
+                    .AppendLine(shippingInfo.ZipCode)
+                    .AppendLine("--_");
+
+                MailMessage mailMessage = new MailMessage(
+                    emailSettings.MailFromAddress,
+                    emailSettings.MailToAddress,
+                    "Shop Good order Placed!",
+                    body.ToString());
+
+                if (emailSettings.WriteAsFile)
+                {
+                    mailMessage.BodyEncoding = Encoding.ASCII;
+                }
+
+                smtpClient.Send(mailMessage);
+            }
+        }
+
+    }
+}
---
+++ ShoppingCartDomain/Entities/EmailSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDomain.Entities
+{
+    public class EmailSettings
+    {
+        public string MailToAddress { get; set; }
+        public string MailFromAddress { get; set; }
+        public bool UseSsl = true;
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string SeverName { get; set; }
+        public int SeverPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"D:\";
+    }
+}
