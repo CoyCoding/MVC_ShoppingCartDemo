4d10fd3de8219b0b4ec5f9be3048543d801a96bd
matt coy <fluffytoycoy@gmail.com>
2018-12-01 18:21:36 -0600

    added layout for crudopperations and controller

---
+++ ShoppingCartDemo/Controllers/AdminCrudController.cs
@@ -0,0 +1,25 @@
+﻿using ShoppingCartDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCartDemo.Controllers
+{
+    public class AdminCrudController : Controller
+    {
+        IApplicationDbContext _ProductRepo;
+
+        public AdminCrudController(IApplicationDbContext productRepo)
+        {
+            _ProductRepo = productRepo;
+        }
+
+        // GET: AdminCrud
+        public ActionResult Index()
+        {
+            return View(_ProductRepo.Products);
+        }
+    }
+}
