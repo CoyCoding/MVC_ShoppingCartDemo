db43e37a4975a031dcac8a376fea8e7bec21dcd0
matt coy <fluffytoycoy@gmail.com>
2018-12-02 20:15:44 -0600

    css and edit view work

--- ShoppingCartDomain/Entities/Product.cs
+++ ShoppingCartDomain/Entities/Product.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace ShoppingCartDomain.Entities
 {
         public class Product
         {
+            [HiddenInput(DisplayValue = false)]
             public int Id { get; set; }
 
             public string Name { get; set; }
 
+            [DataType(DataType.MultilineText)]
             public string Description { get; set; }
 
             public decimal Price { get; set; }
