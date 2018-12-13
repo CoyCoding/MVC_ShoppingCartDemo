4807bf558a175e759778143e265d636198a241c2
matt coy <fluffytoycoy@gmail.com>
2018-12-12 23:13:25 -0600

    UserNames added

--- ShoppingCartDemo/Models/Identity/AccountViewModels.cs
+++ ShoppingCartDemo/Models/Identity/AccountViewModels.cs
@@ -8,6 +8,10 @@ namespace ShoppingCartDemo.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -64,6 +68,10 @@ namespace ShoppingCartDemo.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+            
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
